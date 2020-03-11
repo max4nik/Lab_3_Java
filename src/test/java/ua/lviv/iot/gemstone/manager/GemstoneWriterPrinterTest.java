e7af8882b4ac5ac4d3fd710c78b3477b61dbce57
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-03-11 10:52:22 +0200

    test for reading from and writing to file an information about gemstones

---
+++ src/test/java/ua/lviv/iot/gemstone/manager/GemstoneWriterPrinterTest.java
@@ -0,0 +1,39 @@
+package ua.lviv.iot.gemstone.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ua.lviv.iot.gemstone.model.AbstractGemstone;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GemstoneWriterPrinterTest extends BaseGemstoneInNecklaceTest {
+    private GemstoneWriter gemstoneWriter;
+    private GemstoneReader gemstoneReader;
+    private static final String FILE = "Lab5.txt";
+
+    @BeforeEach
+    public void setUp() {
+        this.gemstoneWriter = new GemstoneWriter();
+        this.gemstoneReader = new GemstoneReader();
+    }
+
+
+    @Test
+    void testWriteToFileAndReadFromFile() throws IOException {
+        List<AbstractGemstone> gemstonesInFile = handmadeNecklace.getGemstonesInLace();
+        try (Writer fileWriter = new FileWriter(FILE)) {
+            this.gemstoneWriter.setWriter(fileWriter);
+            this.gemstoneWriter.writeToFile(gemstonesInFile, FILE);
+        }
+        StringBuilder expectedOutput = new StringBuilder();
+        gemstonesInFile.forEach((gemstone -> expectedOutput.append(gemstone.getHeaders())));
+        gemstonesInFile.forEach((gemstone -> expectedOutput.append(gemstone.toCSV())));
+
+        assertEquals(expectedOutput.toString(), gemstoneReader.readFromFile(FILE).toString());
+    }
+}
