0b379c61d692c1070d6180cd53bedd7f019bc9aa
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-04-25 21:49:55 +0300

    reverted false commit

--- src/main/java/ua/lviv/iot/spring/rest/dataaccess/GemstoneRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.iot.spring.rest.dataaccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.lviv.iot.gemstone.model.Gemstone;
-
-public interface GemstoneRepository extends JpaRepository<Gemstone, Integer> {
-
-}
--- src/main/java/ua/lviv/iot/spring/rest/dataaccess/NecklaceShopRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.iot.spring.rest.dataaccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.lviv.iot.gemstone.model.NecklaceShop;
-
-public interface NecklaceShopRepository extends JpaRepository<NecklaceShop, Integer> {
-
-}
